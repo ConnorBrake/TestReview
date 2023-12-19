586658abfeba80f7b6d3f6929f236c062eb191f4
Connor Brake <153332456+ConnorBrake@users.noreply.github.com>
2023-12-19 10:55:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TestReview
+ 
---
+++ src/main/java/com/mycompany/testreview/Review.java
@@ -0,0 +1,205 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testreview;
+import java.util.Scanner;
+import java.io.File;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.*;
+
+public class Review {
+
+  private static HashMap<String, Double> sentiment = new HashMap<String, Double>();
+  private static ArrayList<String> posAdjectives = new ArrayList<String>();
+  private static ArrayList<String> negAdjectives = new ArrayList<String>();
+  
+  
+  private static final String SPACE = " ";
+  
+  static{
+    try {
+      Scanner input = new Scanner(new File("cleanSentiment.csv"));
+      while(input.hasNextLine()){
+        String[] temp = input.nextLine().split(",");
+        sentiment.put(temp[0],Double.parseDouble(temp[1]));
+        //System.out.println("added "+ temp[0]+", "+temp[1]);
+      }
+      input.close();
+    }
+    catch(Exception e){
+      System.out.println("Error reading or parsing cleanSentiment.csv");
+    }
+  
+  
+  //read in the positive adjectives in postiveAdjectives.txt
+     try {
+      Scanner input = new Scanner(new File("positiveAdjectives.txt"));
+      while(input.hasNextLine()){
+        String temp = input.nextLine().trim();
+        posAdjectives.add(temp);
+      }
+      input.close();
+    }
+    catch(Exception e){
+      System.out.println("Error reading or parsing postitiveAdjectives.txt\n" + e);
+    }   
+ 
+  //read in the negative adjectives in negativeAdjectives.txt
+     try {
+      Scanner input = new Scanner(new File("negativeAdjectives.txt"));
+      while(input.hasNextLine()){
+        negAdjectives.add(input.nextLine().trim());
+      }
+      input.close();
+    }
+    catch(Exception e){
+      System.out.println("Error reading or parsing negativeAdjectives.txt");
+    }   
+  }
+  
+  /** 
+   * returns a string containing all of the text in fileName (including punctuation), 
+   * with words separated by a single space 
+   */
+  public static String textToString( String fileName )
+  {  
+    String temp = "";
+    try {
+      Scanner input = new Scanner(new File(fileName));
+      
+      //add 'words' in the file to the string, separated by a single space
+      while(input.hasNext()){
+        temp = temp + input.next() + " ";
+      }
+      input.close();
+      
+    }
+    catch(Exception e){
+      System.out.println("Unable to locate " + fileName);
+    }
+    //make sure to remove any additional space that may have been added at the end of the string.
+    return temp.trim();
+  }
+  
+  /**
+   * @returns the sentiment value of word as a number between -1 (very negative) to 1 (very positive sentiment) 
+   */
+  public static double sentimentVal( String word )
+  {
+    try
+    {
+      return sentiment.get(word.toLowerCase());
+    }
+    catch(Exception e)
+    {
+      return 0;
+    }
+  }
+  
+  /**
+   * Returns the ending punctuation of a string, or the empty string if there is none 
+   */
+  public static String getPunctuation( String word )
+  { 
+    String punc = "";
+    for(int i=word.length()-1; i >= 0; i--){
+      if(!Character.isLetterOrDigit(word.charAt(i))){
+        punc = punc + word.charAt(i);
+      } else {
+        return punc;
+      }
+    }
+    return punc;
+  }
+  
+  /**
+   * Returns the word after removing any beginning or ending punctuation
+   */
+  public static String removePunctuation( String word )
+  {
+    while(word.length() > 0 && !Character.isAlphabetic(word.charAt(0)))
+    {
+      word = word.substring(1);
+    }
+    while(word.length() > 0 && !Character.isAlphabetic(word.charAt(word.length()-1)))
+    {
+      word = word.substring(0, word.length()-1);
+    }
+    
+    return word;
+  }
+  /** 
+   * Randomly picks a positive adjective from the positiveAdjectives.txt file and returns it.
+   */
+  public static String randomPositiveAdj()
+  {
+    int index = (int)(Math.random() * posAdjectives.size());
+    return posAdjectives.get(index);
+  }
+  
+  /** 
+   * Randomly picks a negative adjective from the negativeAdjectives.txt file and returns it.
+   */
+  public static String randomNegativeAdj()
+  {
+    int index = (int)(Math.random() * negAdjectives.size());
+    return negAdjectives.get(index);
+    
+  }
+  
+  /** 
+   * Randomly picks a positive or negative adjective and returns it.
+   */
+  public static String randomAdjective()
+  {
+    boolean positive = Math.random() < .5;
+    if(positive){
+      return randomPositiveAdj();
+    } else {
+      return randomNegativeAdj();
+    }
+  }
+  
+  //Write your totalSentiment method here. This method should return the total
+  //sentiment value of a review based on an input review
+  public static double totalSentiment(String fileName)
+  {
+    String placeHolder = "";
+    double totalSentiment = 0.0;
+    String review = textToString(fileName);
+    //removes punctuation
+    review = review.replaceAll("\\p{Punct}", "");
+    for (int i = 0; i < review.length(); i++)
+    {
+      if (review.substring(i, i + 1).equals(" "))
+      {
+        totalSentiment += sentimentVal(placeHolder);
+        placeHolder = "";
+      }
+      else
+      {
+        placeHolder += review.substring(i, i + 1);
+      }
+    }
+    return totalSentiment;
+  }
+  
+  //Write your starRating method here. This method should return an int rating from 0-4 based on the total
+  //sentiment value of a review.
+  public static int starRating(String fileName)
+  {
+    int starRating = (int)totalSentiment(fileName);
+    if (starRating <= 0)
+    {
+      starRating = 0;
+    }
+    if (starRating >= 4)
+    {
+      starRating = 4;
+    }
+    return starRating;
+  }
+}
